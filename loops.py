ac7026b162517061a9d8694d758e94d9aaec10ed
Shaban <shabanxhemajli@live.com>
2023-01-11 15:57:34 +0100

    while loops were in menu for today

--- loops.py
+++ loops.py
@@ -26,3 +26,31 @@ print("I dont think they can handle this!")
 for i in range (3):
     print("Hello,hello,hello , how low")
 print("Hello, hello,Hello")
+
+name = input("Enter your name: ")
+while name =="":
+    print("You did not enter your name")
+    name = input("Enter your name: ")
+print(f"Hello {name}")
+
+age = int(input("Enter your age: "))
+while age < 0:
+    print("Age cant be negative ")
+    age = int(input("Enter your age: "))
+
+print(f"You are {age} years old")    
+
+food = input("Enter a fod you like (q to quit)")
+
+while not food == "q":
+    print(f"you like {food}")
+    food = input("Enter another food you like (q to quit)")
+
+print("bye")  
+
+num = int(input("Enter a number between 1-10: "))
+while num < 1 or num > 10:
+    print(f"{num}is not valid")
+    num = int(input("Enter a number between 1-10: "))
+
+print(f"Your number is {num}")
