28bf84768f47f7ef396f47ac9046d45191c4d8b0
Shaban <shabanxhemajli@live.com>
2023-01-13 14:17:51 +0100

    basics:comparison operators in python

---
+++ comparison_operators.py
@@ -0,0 +1,28 @@
+"""
+In Python, comparison operators are used to compare the values of two variables and return a Boolean value (True or False) based on the result of the comparison. The following are the comparison operators in Python:
+
+== (equal to)
+!= (not equal to)
+> (greater than)
+
+< (less than)
+>= (greater than or equal to)
+
+<= (less than or equal to)
+These operators are commonly used in control flow statements, such as if/else statements, to make decisions based on the values of variables.
+"""
+print(3>4)
+print(1==1)
+print(1!=1)
+print(1>=9)
+print(1<1000)
+
+#custom exercise
+x=int(input("Provide 1-st value:"))
+y=int(input("Provide 2-nd value:"))
+print(x==y)
+print(x!=y)
+print(x>y)
+print(x<y)
+print(x<=y)
+print(x>=y)
